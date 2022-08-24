51a402b8ae4441e3a448c9e029e08e7bd183416a
katherinex <kathexio@gmail.com>
2022-08-23 20:42:20 -0500

    Ejercicio de parcial: clasificador de canicas - plantilla

--- canicas.py
+++ canicas.py
@@ -1,31 +1,53 @@
 # ----------------------------------------------------------------------------------------
-# PROGRAMA: <<nombre del programa>>
+# PROGRAMA: Clasificador de canicas
 # ----------------------------------------------------------------------------------------
-# Descripción: <<breve descripción>>
+# Descripción: Este programa se encarga clsificar N cantidad de canicas de acuerdo a los
+# diámetros de cada una de ellas, los cuales son ingresados por el usuario
+
 # ----------------------------------------------------------------------------------------
-# Autor: Luis Carlos Díaz
-# Version: 2.0
-# [18.07.2020]
+# Autores: Lorena Patricia Mora Hernandez - Katherine Xiomar González Santacruz
+# Version: 1.0
+# [23.08.2022]
 # ----------------------------------------------------------------------------------------
-# IMPORTAR MODULOS
-import datetime   # modulo de python para este ejemplo (se usara para mostrar la fecha)
 
 # ----------------------------------------------------------------------------------------
 # VARIABLES GLOBALES Y PRE-CONDICIONES
 # ----------------------------------------------------------------------------------------
 
-# << aqui una explicación concreta >>
+# Variables de entrada: (int) numeroCanicas, (int) diametroCanica, (str) salir
+# pre-condiciones: numeroCanicas, debe ser un entero mayor o igual que 3 y menor o igual que 10
+#                  diametroCanica, cada valor ingresado debe ser int
+#                  salir != 'si'
+
+# Variables auxiliares: (int) numeroMultiplos, (list) listaDiametros, (int) totalBuenas,
+#                       (int) totalPotBuenas, (int) totalMalas, (bool) seguirPreguntando
+#
+# Explicación: Si salir es 'si', el programa dejará de ejecutarse.
+#              numeroCanicas, representa el número de canicas que el usuario quiere clasificar.
+#              diametroCanica, cada uno de los diámetros de las canicas ingresados por el usuario.
+#              numeroMultiplos, variable auxiliar para contar cuantos múltiplos tiene un diametro.
+#              listaDiametros, variable para almacenar la lista de todos los diámetros ingresados por el usuario.
+#              totalBuenas, variable auxiliar para contar el número de canicas buenas,
+#              totalPotBuenas, variable auxiliar para contar el número de canicas potencialmente buenas,
+#              totalMalas, variable auxiliar para contar el número de canicas malas,
+#              seguirPreguntando, es una variable auxiliar para saber si el programa debe volver a preguntar: sobre
+#              ingresar un dato, o si desea salir del programa (dependiendo del caso en que se use)
 
 # ----------------------------------------------------------------------------------------
 # POSTCONDICIONES
 # ----------------------------------------------------------------------------------------
 
-# << aqui una explicación concreta >>
+# Salida: Mensaje (str) informando la clasificación de cada una de las canicas y el numero de canicas buenoas,
+# potencialmente buenas y malas.
+
+# Si salir == 'si' , fin del programa.
 
 # ----------------------------------------------------------------------------------------
 # PARAMETROS
 # ----------------------------------------------------------------------------------------
-# listar aqui los parámetros
+# buena, potBuena, mala, son parámetros para almacenar los tipos de clasificacion
+
+# ----------------------------------------------------------------------------------------
 
 buena = 'Buena'
 potBuena = 'Potencialmente buena'
@@ -83,7 +105,7 @@ def validarDiametroCanica (mensaje):
 def getDiametrosCanicas (numeroCanicas):
     listaDiametros = []
     for i in range(numeroCanicas):
-        diametroCanica = validarDiametroCanica('Ingrese el valor del diametro de la canica número ' + str(i + 1) +': ')
+        diametroCanica = validarDiametroCanica('Ingrese el valor del diámetro de la canica número ' + str(i + 1) +': ')
         listaDiametros.append(diametroCanica)
     return listaDiametros
 
